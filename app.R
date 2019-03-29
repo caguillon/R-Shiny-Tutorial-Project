c9e55e1d88541c19872729b669f5de0e1a9f7aa4
Cynthia Aguillon <caguillon.techie@gmail.com>
2019-03-29 13:28:23 -0500

    Working with inputs

--- app.R
+++ app.R
@@ -1,7 +1,23 @@
 #required libraries
 library(shiny)
 
-#initializes an empty UI and an empty serve
-ui <- fluidPage()
-server <- function(input, output, session) {}
+#reading in dataset
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+#initializes an empty UI and an empty server:
+
+#R strings inside fluidPage() to render text
+ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
+  sidebarLayout(
+    sidebarPanel(sliderInput("priceInput", "Price", min = 0, max = 100,
+                             value = c(25, 40), pre = "$")
+                 ),
+    mainPanel("the results will go here")
+  )
+)
+server <- function(input, output, session) {
+  print(str(bcl))
+}
+
 shinyApp(ui = ui, server = server)

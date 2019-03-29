cd0a71f064ec18810d6aca980a953131aabb52e2
Cynthia Aguillon <caguillon.techie@gmail.com>
2019-03-29 11:08:44 -0500

    method two to make an app

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 #required libraries
 library(shiny)
 
+#initializes an empty UI and an empty serve
 ui <- fluidPage()
 server <- function(input, output, session) {}
 shinyApp(ui = ui, server = server)
